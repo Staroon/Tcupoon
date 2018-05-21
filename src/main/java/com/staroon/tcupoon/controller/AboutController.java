7c6ba8f11fa8f419c839b7b80820803e71bf1d6d
Staroon <staroon@outlook.com>
2018-05-21 17:05:03 +0800

    关于界面调整

--- src/main/java/com/staroon/tcupoon/controller/AboutController.java
+++ src/main/java/com/staroon/tcupoon/controller/AboutController.java
@@ -1,7 +1,11 @@
 package com.staroon.tcupoon.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
 
+import java.net.URI;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -12,8 +16,22 @@ import java.util.ResourceBundle;
  * Time: 15:33
  */
 public class AboutController implements Initializable {
+    @FXML
+    private Hyperlink toGit;
+    @FXML
+    private Hyperlink toBlog;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void toGit(ActionEvent action) throws Exception {
+        java.awt.Desktop.getDesktop().browse(new URI("https://github.com/Staroon"));
+
+    }
+
+    public void toBlog(ActionEvent action) throws Exception {
+        java.awt.Desktop.getDesktop().browse(new URI("https://staroon.pro"));
     }
 }
